cb9fec349192654c94894586838d8f5bd970c27e
HernanFaustino <chaccachuctaya@gmail.com>
2023-04-27 21:51:44 -0500

    fix: Save Github key in env file

--- src/pages/api/commits/index.ts
+++ src/pages/api/commits/index.ts
@@ -5,15 +5,15 @@ type Data = {
   status: 'failed' | 'aborted' | 'success';
   data: Commit[]
 }
+
 const octokit = new Octokit({
-  auth: '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'
+  auth: process.env.GITHUB_KEY
 })
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
  const response = await octokit.request('GET /repos/{owner}/{repo}/commits', {
     owner: 'HernanFaustino',
     repo: 'github-dashboard',
